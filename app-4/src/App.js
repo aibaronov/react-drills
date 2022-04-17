7fef5bb65e53e763a49b7031f41441b5c2008d1a
Arthur Baronov <abaronov1234@gmail.com>
2022-04-17 15:41:32 -0600

    Completed app-4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  function userNameHandler(event){
+    setUserName(event.target.value);
+  }
+
+  function passwordHandler(event){
+    setPassword(event.target.value);
+  }
+
+  function submitHandler(event){
+
+    event.preventDefault();
+    alert(`Username: ${userName} and Password: ${password}`)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={userNameHandler}></input>
+      <input onChange={passwordHandler}></input>
+      <button onClick={submitHandler}>Submit</button>
     </div>
   );
 }
