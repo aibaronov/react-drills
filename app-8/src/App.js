19a18e192a3287a8a1497e156cc1d0c3c8130aa2
Arthur Baronov <abaronov1234@gmail.com>
2022-04-19 21:19:46 -0600

    Completed app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      psyduckName: '',
+      psyduckAbilities: []
+    }
+  }
+  componentDidMount(){
+    axios.get('https://pokeapi.co/api/v2/pokemon/psyduck').then((res) => {
+      console.log(res.data);
+      this.setState({
+        psyduckName: res.data.name,
+        psyduckAbilities: res.data.abilities,
+      })
+    })
+  }
+
+  render(){
+    const abilities = this.state.psyduckAbilities;
+    const allAbilities = abilities.map(ability => ability.ability.name);
+    console.log(allAbilities);
+    return (
+      <div>
+        <h1>Pokemon Name: {this.state.psyduckName}</h1>
+        <h3>List of abilities:</h3>
+        {allAbilities.map(ability => {
+          return (
+            <li>{ability}</li>
+          )
+        })}
+
+      </div>
+    )
+  }
 }
 
 export default App;
